72991d4818664865fd0f64514e315f6792bafe51
innocenthunter556-lab <innocenthunter556@gmail.com>
2026-08-02 22:53:41 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Web-bros',
+  description: 'Web-bros Landing Page',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: 'system-ui, sans-serif' }}>
+        {children}
+      </body>
+    </html>
+  )
+}
+
