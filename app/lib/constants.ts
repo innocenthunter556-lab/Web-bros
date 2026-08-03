bc5dbb64648ef8f777221319a61095c3f349dcfb
innocenthunter556-lab <innocenthunter556@gmail.com>
2026-08-03 20:57:35 +0530

    Update constants.ts

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,1 @@
-https://wa.me/916300424639?text=Hi %20Webros%2C%201%27m %20interested%20in%20Websites %20%26%20Web%20Apps.
+export const waLink = "https://wa.me/916300424639?text=Hi%20Webros%2C%20I%27m%20interested%20in%20Websites%20%26%20Web%20Apps.";
