0f9c7168c6d1b11e2f0b0fcc5b4f0ef525cee933
innocenthunter556-lab <innocenthunter556@gmail.com>
2026-08-03 20:33:11 +0530

    Update WhatsApp link in constants.ts

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,3 +1,1 @@
-// lib/constants.ts
-export const waLink = "https://wa.me/+91 86390 07613"; // Replace with your actual WhatsApp link
-
+https://wa.me/916300424639?text=Hi %20Webros%2C%201%27m %20interested%20in%20Websites %20%26%20Web%20Apps.
